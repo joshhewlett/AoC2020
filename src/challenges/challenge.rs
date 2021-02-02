c75fdb1aa24732eca0904bf5576610cfbd7e6a81
Josh <josh.hewlett@gmail.com>
2021-02-01 20:07:39 -0700

    Combined day 1 part 1 and 2

---
+++ src/challenges/challenge.rs
@@ -0,0 +1,9 @@
+struct Challenge {
+    name: String,
+    how_many_parts: u8
+}
+
+trait RunChallenge {
+
+    fn run();
+}
--- src/challenges/day01.rs
+++ src/challenges/day01.rs
@@ -9,44 +9,115 @@ pub fn run() {
         .map(|x| x.trim().parse::<i32>().unwrap())
         .collect();
 
-    println!("Input data size: {}", data.len());
-
     data.sort();
 
-    let mut result = None;
-
-    // Loop through data elements and try to find a counterpart
-    for (i, _) in data.iter().enumerate() {
-        result = find_addend_indexes(&data, i);
-
-        if result.is_some() {
-            break;
-        }
-    }
+    // Get result
+    let part_1_result = find_addend_indexes(&data, 2);
+    let part_2_result = find_addend_indexes(&data, 3);
 
     // Output results
-    if result.is_some() {
-        println!(
-            "Challenge result: {}",
-            data[result.unwrap().0] * data[result.unwrap().1]
-        );
+    if part_1_result.is_some() {
+        println!("=== Challenge Part 1 result: ");
+
+        print!("Addends: ");
+        let mut multiplied_value: i32 = 1;
+        for i in part_1_result.unwrap() {
+            print!("{} ", data[i]);
+            multiplied_value *= data[i];
+        }
+        println!("\nProduct of addends: {}", multiplied_value);
     } else {
-        println!("Challenge result: Result not found");
+        println!("Challenge Part 1 result: Result not found");
+    }
+
+    println!("");
+    if part_2_result.is_some() {
+        println!("=== Challenge Part 2 result: ");
+
+        print!("Addends: ");
+        let mut multiplied_value: i32 = 1;
+        for i in part_2_result.unwrap() {
+            print!("{} ", data[i]);
+            multiplied_value *= data[i];
+        }
+        println!("\nProduct of addends: {}", multiplied_value);
+    } else {
+        println!("Challenge Part 2 result: Result not found");
     }
 }
 
+fn find_addend_indexes(array_to_search: &Vec<i32>, how_many_addends: u8) -> Option<Vec<usize>> {
+    return find_addend_indexes_recursive(
+        array_to_search,
+        how_many_addends - 1,
+        &mut Vec::new(),
+        0,
+    );
+}
+
 /**
- * Returns a tuple of indexes whose values added together equals SUM_TO_FIND
+ * Find (depth + 1) number of indexes who's values in array_to_search are added together, equal
+ * SUM_TO_FIND. Algorithm is pretty inefficient/brute-force. Possible permutations of algorithm is
+ * "(array_to_search.len()) choose (depth + 1)" or:
+ *
+ * When n = array_to_search.len() and r = depth + 1:
+ * (r + n - 1)!/(r!(n - 1))!
+ *
+ * (It's a lot)
  */
-fn find_addend_indexes(array: &Vec<i32>, index: usize) -> Option<(usize, usize)> {
-    let counterpart_addend = SUM_TO_FIND - array[index];
+fn find_addend_indexes_recursive(
+    array_to_search: &Vec<i32>,
+    depth: u8,
+    result: &mut Vec<usize>,
+    current_index: usize,
+) -> Option<Vec<usize>> {
+    result.push(current_index);
 
-    let search_result = array.binary_search(&counterpart_addend);
-
-    // If there was no result or the indexes are the same, return None
-    if search_result.is_err() || search_result.ok().unwrap() == index {
+    // The first (depth) values in result has exhausted their options. Return None
+    if depth == 0 && current_index == array_to_search.len() {
+        result.pop();
         return None;
     }
+    // Solution was found. Return result
+    else if depth == 0 && get_sum_of_arr(array_to_search, result) == SUM_TO_FIND {
+        return Some(result.to_vec());
+    }
+    // Solution was not found in this current iteration. Increment current_index and try again at same depth
+    else if depth == 0 && current_index < (array_to_search.len() - 1) {
+        result.pop();
+        return find_addend_indexes_recursive(array_to_search, depth, result, current_index + 1);
+    }
+    // Dig deeper and find solution
+    else if depth > 0 && current_index < (array_to_search.len() - depth as usize) {
+        let sub_result =
+            find_addend_indexes_recursive(array_to_search, depth - 1, result, current_index + 1);
 
-    return Some((index, search_result.ok().unwrap()));
+        // If sub_result wasn't found, increment current_index at current depth and try again
+        if sub_result.is_none() {
+            result.pop();
+            return find_addend_indexes_recursive(
+                array_to_search,
+                depth,
+                result,
+                current_index + 1,
+            );
+        }
+
+        // Solution was found. Return sub_result
+        return sub_result;
+    }
+
+    // No solution was found at current depth/current_index
+    result.pop();
+    return None;
+}
+
+// Get sum of values at indexes' positions in array
+fn get_sum_of_arr(array: &Vec<i32>, indexes: &Vec<usize>) -> i32 {
+    let mut sum = 0;
+    for i in indexes {
+        sum += array[*i];
+    }
+
+    return sum;
 }
